84a735b4dbbd0be7484138d68880384ce32d242e
Stanislav Rush <911rush@gmail.com>
2023-02-04 17:37:35 +0700

    Добавлено меню и заглушки для него
    Resolve https://github.com/stas12312/my-tickets-bot/issues/4

--- my_tickets_bot/src/bot/handlers/__init__.py
+++ my_tickets_bot/src/bot/handlers/__init__.py
@@ -1,2 +1,13 @@
 """Обработчики"""
+from aiogram import Router
+
 from .common import common_handlers
+from .settings import settings_router
+from .tickets import tickets_handler
+
+# Главный роутер
+main_router = Router()
+
+main_router.include_router(common_handlers)
+main_router.include_router(settings_router)
+main_router.include_router(tickets_handler)
