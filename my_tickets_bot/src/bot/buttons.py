05dff4ddf0f2c276c8fbfbc9735ef879b4329521
Stanislav Rush <911rush@gmail.com>
2023-02-08 23:40:18 +0700

    Изменение "Билеты" на "Мероприятия" (#53)
    Resolve https://github.com/stas12312/my-tickets-bot/issues/52

--- my_tickets_bot/src/bot/buttons.py
+++ my_tickets_bot/src/bot/buttons.py
@@ -4,19 +4,19 @@ from enum import StrEnum
 
 class MainMenu(StrEnum):
     """Главное меню"""
-    MY_TICKETS = '🎫 Мои билеты'
-    ADD_TICKET = '🆕 Добавить билет'
+    MY_EVENTS = '✨ Мои мероприятия'
+    ADD_EVENT = '➕ Добавить мероприятие'
     SETTINGS = '⚙ Настройки'
 
 
 class Settings(StrEnum):
     """Настройки"""
     MY_CITIES = '🏙 Мои города'
-    ADD_CITY = '🆕 Добавить город'
+    ADD_CITY = '➕ Добавить город'
     DELETE_CITY = '🗑 Удалить город'
 
     MY_PLACES = '🎭 Мои места'
-    ADD_LOCATION = '🆕 Добавить место'
+    ADD_LOCATION = '➕ Добавить место'
     DELETE_LOCATION = '🗑 Удалить место'
 
     BACK = '◀️ Назад'
