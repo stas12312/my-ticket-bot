d0a65e8ca2c752568ae6d0d0d98b880cc99e669d
Stanislav Rush <911rush@gmail.com>
2023-02-09 23:10:47 +0700

    Реализовано удаление билета
    https://github.com/stas12312/my-tickets-bot/issues/57

--- my_tickets_bot/src/bot/handlers/ticket.py
+++ my_tickets_bot/src/bot/handlers/ticket.py
@@ -1,3 +1,4 @@
+"""Обработчики для билетов"""
 from aiogram import Router, F, Bot
 from aiogram import types
 from aiogram.fsm.context import FSMContext
@@ -5,7 +6,7 @@ from aiogram.types import ContentType
 
 from bot.callbacks import TicketCallback, EntityAction
 from bot.forms import TicketForm
-from bot.keybaords import get_actions_for_event
+from bot.keybaords import get_actions_for_event, get_actions_for_ticket
 from bot.messages import make_event_message
 from bot.utils import get_file_type, FileType, save_ticket
 from services.repositories import Repo
@@ -18,15 +19,26 @@ async def download_ticket_handler(
         bot: Bot,
 ):
     """Обработка скачивание билета"""
+
     ticket_id = callback_data.ticket_id
+    event_id = callback_data.event_id
     ticket = await repo.ticket.get(query.from_user.id, ticket_id)
+
+    if ticket is None:
+        # Редактируем текущее сообщение для актуализации
+        event = await repo.event.get(query.from_user.id, event_id)
+        tickets = await repo.ticket.list_for_event(query.from_user.id, event_id)
+        msg = make_event_message(event)
+        keyboard = get_actions_for_event(event, tickets)
+        await query.message.edit_text(msg, reply_markup=keyboard, disable_web_page_preview=True)
 
     file = await bot.get_file(ticket.file.bot_file_id)
     await query.answer()
+    keyboard = get_actions_for_ticket(ticket)
     if get_file_type(file) == FileType.PHOTO:
-        await query.message.answer_photo(ticket.file.bot_file_id)
+        await query.message.answer_photo(ticket.file.bot_file_id, reply_markup=keyboard)
     else:
-        await query.message.answer_document(ticket.file.bot_file_id)
+        await query.message.answer_document(ticket.file.bot_file_id, reply_markup=keyboard)
 
 
 async def start_add_ticket_handler(
@@ -35,8 +47,8 @@ async def start_add_ticket_handler(
         state: FSMContext,
 ):
     """Начало добавление билета"""
-
     event_id = callback_data.event_id
+
     await state.set_state(TicketForm.file)
     await state.update_data(event_id=event_id)
     await query.message.answer('Отправьте файл билета')
@@ -71,6 +83,20 @@ async def save_ticket_handler(
     )
 
 
+async def delete_ticket_handler(
+        query: types.CallbackQuery,
+        callback_data: TicketCallback,
+        repo: Repo,
+
+):
+    """Удаление билета"""
+    ticket_id = callback_data.ticket_id
+
+    await repo.ticket.delete(query.from_user.id, ticket_id)
+    await query.message.delete()
+    await query.answer('Билет удален')
+
+
 tickets_handler = Router()
 tickets_handler.callback_query.register(download_ticket_handler, TicketCallback.filter(F.action == EntityAction.show))
 tickets_handler.callback_query.register(start_add_ticket_handler, TicketCallback.filter(F.action == EntityAction.add))
@@ -79,3 +105,4 @@ tickets_handler.message.register(
     TicketForm.file,
     F.content_type.in_([ContentType.DOCUMENT, ContentType.PHOTO]),
 )
+tickets_handler.callback_query.register(delete_ticket_handler, TicketCallback.filter(F.action == EntityAction.delete))
