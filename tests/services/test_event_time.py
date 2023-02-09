96031a45c9b5187b3636370a52714a8a3d7f4ff0
Stanislav Rush <911rush@gmail.com>
2023-02-10 00:50:36 +0700

    Исправлено отображение минут
    Resolve https://github.com/stas12312/my-tickets-bot/issues/63

--- tests/services/test_event_time.py
+++ tests/services/test_event_time.py
@@ -7,14 +7,14 @@ from services.event_time import get_beatify_datetime
 
 testdata = [
     (6, 2, 23, 14, 10, '6 Февраля в 14:10 (Пн)'),
-    (7, 8, 22, 9, 20, '7 Августа в 9:20 (Вс)'),
+    (7, 8, 22, 9, 2, '7 Августа в 9:02 (Вс)'),
 ]
 
 
 @pytest.mark.parametrize('day,month,year,hour,minute,result', testdata)
 def test_beautify_datetime(day: int, month: int, year: int, hour: int, minute: int, result):
     """Проверка преобразования даты в красивый вид"""
-    dt = datetime.datetime(year, month, day, hour, minute)
+    datetime_ = datetime.datetime(year, month, day, hour, minute)
 
-    beautify_time = get_beatify_datetime(dt)
+    beautify_time = get_beatify_datetime(datetime_)
     assert beautify_time == result
