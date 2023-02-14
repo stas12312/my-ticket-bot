ca6abc33517adce9b644bc70f3547d499f79f036
Stanislav Rush <911rush@gmail.com>
2023-02-15 00:37:03 +0700

    Добавлено отображение оставшееся до мероприятия время (#91)
    Resolve https://github.com/stas12312/my-tickets-bot/issues/45

---
+++ my_tickets_bot/src/services/localization.py
@@ -0,0 +1,15 @@
+"""Работа с локализацией"""
+
+
+def plural(
+        number: str,
+        *args,
+) -> str:
+    """Выбор окончания для числа"""
+    word = args[2]
+    if number % 10 in {2, 3, 4}:
+        word = args[1]
+    if number % 10 == 1 and number != 11:
+        word = args[0]
+
+    return f'{number} {word}'
