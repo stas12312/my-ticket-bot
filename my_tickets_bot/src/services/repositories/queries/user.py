e0e684c80ad6e50e93dba9300f8514bda74bd5b4
Stanislav Rush <911rush@gmail.com>
2023-02-19 00:33:08 +0700

    Добавлено сохранение имени и фамилии пользователя (#119)
    Resolve https://github.com/stas12312/my-tickets-bot/issues/118

--- my_tickets_bot/src/services/repositories/queries/user.py
+++ my_tickets_bot/src/services/repositories/queries/user.py
@@ -2,8 +2,8 @@
 
 SAVE_USER = """
     INSERT 
-    INTO "user" (id, username) VALUES ($1, $2)
+    INTO "user" (id, username, first_name, last_name) VALUES ($1, $2, $3, $4)
     ON CONFLICT (id) DO UPDATE
-    SET id=$1, username=$2
+    SET id=$1, username=$2, first_name=$3, last_name=$4
     RETURNING *
     """
--- my_tickets_bot/src/services/repositories/user.py
+++ my_tickets_bot/src/services/repositories/user.py
@@ -1,3 +1,4 @@
+"""Репозиторий пользователя"""
 from asyncpg import Connection, Record
 
 from .queries import user
@@ -16,6 +17,8 @@ class UserRepo:
             self,
             user_id: int,
             username: str | None,
+            first_name: str | None,
+            last_name: str | None,
     ) -> Record:
         """Сохранение пользователя"""
-        return await self._conn.fetchrow(user.SAVE_USER, user_id, username)
+        return await self._conn.fetchrow(user.SAVE_USER, user_id, username, first_name, last_name)
