0716d2837c55fd3809139799f170897d7c185eda
Stanislav Rush <911rush@gmail.com>
2023-02-21 22:08:19 +0700

    Реализована отправка сообщения о завершении обновления
    Fixed https://github.com/stas12312/my-tickets-bot/issues/128

--- my_tickets_bot/src/bot/utils.py
+++ my_tickets_bot/src/bot/utils.py
@@ -1,12 +1,14 @@
 """Вспомогательные функции"""
 from enum import IntEnum
 
+from aiogram import Bot
 from aiogram.types import File, Message
 
 from services.repositories import Repo
 
 
 class FileType(IntEnum):
+    """Типы файлов"""
     PHOTO = 0
     DOCUMENT = 1
 
@@ -30,3 +32,26 @@ async def save_ticket(
 
     ticket = await repo.ticket.save(event_id)
     await repo.file.save_file(ticket.ticket_id, None, file_id)
+
+
+async def send_message_for_users(
+        bot: Bot,
+        user_ids: list[int],
+        message: str,
+):
+    """Отправка сообщения пользователям"""
+    for user_id in user_ids:
+        await safe_send_message(bot, user_id, message)
+
+
+async def safe_send_message(
+        bot,
+        user_id: int,
+        message: str,
+):
+    """Безопасная отправка сообщения"""
+
+    await bot.send_message(
+        chat_id=user_id,
+        text=message,
+    )
