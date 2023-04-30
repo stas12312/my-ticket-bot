3e8cc57802b1861a8ecd1dd8b117fb5d04bb9b84
Stanislav Rush <911rush@gmail.com>
2023-04-30 12:48:10 +0700

    Исправления в парсере для "Камерный театр Малыщицкого"
    Resolved https://github.com/stas12312/my-tickets-bot/issues/224

--- tests/parsers/spb/test_malyshchitsky.py
+++ tests/parsers/spb/test_malyshchitsky.py
@@ -21,3 +21,14 @@ async def test_malyshchitsky_parser():
     events = await parser.get_events()
 
     assert len(events) == 2
+    print(events)
+
+    assert events[0].name == 'ГЕРБ ГОРОДА ЭН'
+    assert events[0].url == 'https://www.vmtheatre.ru/gerb'
+    assert events[0].datetime == datetime.datetime(2023, 4, 29, 19, 00)
+
+    assert events[1].name == 'СИРЕНЫ ТИТАНА'
+    assert events[1].url == 'https://www.vmtheatre.ru/sireny'
+    assert events[1].datetime == datetime.datetime(2023, 4, 30, 19, 00)
+
+
